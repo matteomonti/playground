c9359ac186332e66427c58dae67d0e6e3843b108
Ubuntu <ubuntu@ip-172-31-2-240.eu-central-1.compute.internal>
2023-02-10 15:49:20 +0000

    Wow. Massive speedup.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use madvise::{madvise, AccessPattern};
 use memmap::MmapOptions;
 use std::{fs::File, hint::black_box, iter, os::unix::prelude::MetadataExt, time::Instant};
 
 fn gather(bytes: &[u8], block_size: usize, reads: &[usize]) -> Vec<u8> {
     let mut gathered = Vec::with_capacity(reads.len() * block_size);
+
+    for read in reads {
+        unsafe {
+            madvise(
+                bytes.as_ptr().offset((read * block_size) as isize),
+                block_size,
+                AccessPattern::WillNeed,
+            )
+            .unwrap();
+        }
+    }
 
     for read in reads {
         gathered.extend_from_slice(&bytes[(read * block_size)..((read + 1) * block_size)]);
@@ -12,13 +24,22 @@ fn gather(bytes: &[u8], block_size: usize, reads: &[usize]) -> Vec<u8> {
 }
 
 fn main() {
-    let file = File::open("/home/mmonti/blob.bin").unwrap();
+    let file = File::open("/home/ubuntu/blob.bin").unwrap();
     let metadata = file.metadata().unwrap();
 
-    let block_size = metadata.blksize() as usize;
-    let blocks = metadata.len() as usize / block_size;
+    let length = metadata.len() as usize;
+    let block_size = metadata.blksize() as usize * 2;
+    let blocks = length / block_size;
+
+    println!("Block size: {block_size}");
+    println!("Number of blocks: {blocks}");
 
     let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+
+    unsafe {
+        madvise(mmap.as_ptr(), length, AccessPattern::Random).unwrap();
+    }
+
     let reads = iter::repeat_with(|| rand::random::<usize>() % blocks)
         .take(32000)
         .collect::<Vec<_>>();
